774b5695931a6e1ba4826dd1ceb4b4df3e9e8905
teampapat <128067204+teampapat@users.noreply.github.com>
2023-03-17 16:15:41 +0700

    Add files via upload

---
+++ simas_psdm/views/templates/sidebar.php
@@ -0,0 +1,98 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="icon-folder menu-icon"></i>
+              <span class="menu-title">Master Data</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="datasiswa.html">Siswa</a></li>
+                <li class="nav-item"> <a class="nav-link" href="dataguru.html">Guru</a></li>
+                <li class="nav-item"> <a class="nav-link" href="datakaryawan.html">Karyawan</a></li>
+              </ul>
+            </div>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+              <i class="icon-columns menu-icon"></i>
+              <span class="menu-title">Humas</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="form-elements">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"><a class="nav-link" href="isihumas.html">kosong</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#kesiswaan" aria-expanded="false" aria-controls="kesiswaan">
+              <i class="icon-bar-graph menu-icon"></i>
+              <span class="menu-title">Kesiswaan</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="kesiswaan">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="isikesiswaan.html">kosong</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#kurikulum" aria-expanded="false" aria-controls="kurikulum">
+              <i class="icon-grid-2 menu-icon"></i>
+              <span class="menu-title">Kurikulum</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="kurikulum">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="kurikulum.html">kosong</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
+              <i class="icon-bar-graph menu-icon"></i>
+              <span class="menu-title">SDM</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="charts">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="sdmstruktur.html">Struktur Organisasi</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sdmprestasiguru.html">Prestasi Guru</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
+              <i class="icon-grid-2 menu-icon"></i>
+              <span class="menu-title">SARPRAS</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="tables">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="isisarpras.html">kosong</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
+              <i class="icon-contract menu-icon"></i>
+              <span class="menu-title">Full Element</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="icons">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Here</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </nav>
