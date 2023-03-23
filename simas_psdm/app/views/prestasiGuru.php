65ef4e4f52780ff71dd62e1e5d3ea0a2b709daee
teampapat <128067204+teampapat@users.noreply.github.com>
2023-03-23 15:46:12 +0700

    Add files via upload

---
+++ simas_psdm/app/views/prestasiGuru.php
@@ -0,0 +1,182 @@
+<div class="isi">
+  <div class="col-lg-12 grid-margin stretch-card">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Prestasi Guru</h4>
+              <p class="card-description">
+                
+              </p>
+              <div  style="width: 96%;">
+                <table class="table table-bordered"style="width: 100%; border: radius 15px;">
+                  <thead>
+                    <tr>
+                      <th>
+                        #
+                      </th>
+                      <th>
+                        Prestasi
+                      </th>
+                      <th>
+                        Skala
+                      </th>
+                      <th>
+                        Juara
+                      </th>
+                      <th>
+                        Tahun
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        1
+                      </td>
+                      <td>
+                        Kepala Sekolah Terbaik Tingkat Provinsi
+                      </td>
+                      <td>
+                        Provinsi Jawa Timur
+                        </div>
+                      </td>
+                      <td>
+                        1
+                      </td>
+                      <td>
+                        2018
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        2
+                      </td>
+                      <td>
+                        Messsy Adam
+                      </td>
+                      <td>
+                        <div class="progress">
+                          <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                      </td>
+                      <td>
+                        $245.30
+                      </td>
+                      <td>
+                        July 1, 2015
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        3
+                      </td>
+                      <td>
+                        John Richards
+                      </td>
+                      <td>
+                        <div class="progress">
+                          <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                      </td>
+                      <td>
+                        $138.00
+                      </td>
+                      <td>
+                        Apr 12, 2015
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        4
+                      </td>
+                      <td>
+                        Peter Meggik
+                      </td>
+                      <td>
+                        <div class="progress">
+                          <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                      </td>
+                      <td>
+                        $ 77.99
+                      </td>
+                      <td>
+                        May 15, 2015
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        5
+                      </td>
+                      <td>
+                        Edward
+                      </td>
+                      <td>
+                        <div class="progress">
+                          <div class="progress-bar bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                      </td>
+                      <td>
+                        $ 160.25
+                      </td>
+                      <td>
+                        May 03, 2015
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        6
+                      </td>
+                      <td>
+                        John Doe
+                      </td>
+                      <td>
+                        <div class="progress">
+                          <div class="progress-bar bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                      </td>
+                      <td>
+                        $ 123.21
+                      </td>
+                      <td>
+                        April 05, 2015
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        7
+                      </td>
+                      <td>
+                        Henry Tom
+                      </td>
+                      <td>
+                        <div class="progress">
+                          <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                      </td>
+                      <td>
+                        $ 150.00
+                      </td>
+                      <td>
+                        June 16, 2015
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+      <!-- tabel -->
+        
+        
+      
+
+            
+          
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+</div>
