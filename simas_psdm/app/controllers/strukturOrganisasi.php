fce4b9ceae34524a0b536248f3976b9397c23d50
teampapat <128067204+teampapat@users.noreply.github.com>
2023-03-28 09:34:32 +0700

    Update strukturOrganisasi.php

--- simas_psdm/app/controllers/strukturOrganisasi.php
+++ simas_psdm/app/controllers/strukturOrganisasi.php
@@ -1,23 +1,23 @@
 <?php
-    class Struktur_organisasi extends Controller{
+    class strukturOrganisasi extends Controller{
         public function index(){
             $data['judul'] = 'Struktur Organisasi';
-            $data['organisasi'] = $this->model('strukturOrganisasi_models')->getALLOrganisasi();
+            $data['organisasi'] = $this->model('strukturOrganisasi_models')->getALLStruktur();
             $this->view('templates/header', $data);
-            $this->view('organisasi/index', $data);
+            $this->view('strukturOrganisasi/index', $data);
             $this->view('templates/footer');
         }
 
         public function detail($id){
             $data['judul'] = 'Detail Struktur Guru';
-            $data['organisasi'] = $this->model('strukturOrganisasi_models')->getOrganisasiById($id);
+            $data['organisasi'] = $this->model('strukturOrganisasi_models')->getStrukturById($id);
             $this->view('templates/header', $data);
-            $this->view('organisasi/detail', $data);
+            $this->view('strukturOrganisasi/detail', $data);
             $this->view('templates/footer');
         }
 
         public function tambah(){
-            if ( $this->model('strukturOrganisasi_models')->tambahDataOrganisasi($_POST) > 0){
+            if ( $this->model('strukturOrganisasi_models')->tambahDataStruktur($_POST) > 0){
                 Flasher::setFlash('berhasil', 'ditambahkan', 'success');
                 header('Location: ' . BASEURL . '/organisasi');
                 exit;
@@ -29,7 +29,7 @@
         }
 
         public function hapus($id){
-        if( $this->model('strukturOrganisasi_models')->hapusDataOrganisasi($id) > 0 ) {
+        if( $this->model('strukturOrganisasi_models')->hapusDataStruktur($id) > 0 ) {
             Flasher::setFlash('berhasil', 'dihapus', 'success');
             header('Location: ' . BASEURL . '/organisasi');
             exit;
@@ -41,7 +41,7 @@
     }
 
     public function getubah(){
-        echo json_encode($this->model('strukturOrganisasi_models')->getOrganisasiById($_POST['id']));
+        echo json_encode($this->model('strukturOrganisasi_models')->getSrukturById($_POST['id']));
     }
 
     public function ubah(){
